f0e4c8c523f4ee89276a0fd349da64fd3a7836ef
miszalemko <lemkomisza@gmail.com>
2018-03-11 12:08:30 +0100

    Creating connection JSP with model by set/get atribute and dispatcher

--- src/twitter/controlers/TweetsControler.java
+++ src/twitter/controlers/TweetsControler.java
@@ -2,6 +2,7 @@ package twitter.controlers;
 
 import twitter.dto.TwitterMessageDto;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,15 +13,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "TweetsController", value = "/tweets")
-public class TweetsControler extends HttpServlet{
+public class TweetsControler extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TwitterMessageDto twitterMessageDto1 = new TwitterMessageDto(1,"First message","Mike");
-        TwitterMessageDto twitterMessageDto2 = new TwitterMessageDto(2,"Second message","John");
+        TwitterMessageDto twitterMessageDto1 = new TwitterMessageDto(1, "First message", "Mike");
+        TwitterMessageDto twitterMessageDto2 = new TwitterMessageDto(2, "Second message", "John");
 
         List<TwitterMessageDto> tweets = new ArrayList<TwitterMessageDto>();
         tweets.add(twitterMessageDto1);
         tweets.add(twitterMessageDto2);
+
+        req.setAttribute("tweetsVodel", tweets);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/tweets/tweets.jsp");
+        dispatcher.forward(req, resp);
     }
 }
+
---
+++ web/tweets/tweets.jsp
@@ -0,0 +1,33 @@
+<%@ page import="java.util.List" %>
+<%@ page import="twitter.dto.TwitterMessageDto" %><%--
+  Created by IntelliJ IDEA.
+  User: maykloff
+  Date: 11.03.2018
+  Time: 11:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List of tweets</title>
+</head>
+<body>
+<div>List of tweets</div>
+<ul>
+    <%
+        List<TwitterMessageDto> tweets = (List<TwitterMessageDto>) request.getAttribute("tweetsModel");
+        for (TwitterMessageDto twitterMessageDto : tweets) {
+    %>
+    <li>
+        <div>
+            id: <%=twitterMessageDto.getId()%>
+            message: <%=twitterMessageDto.getMessage()%>
+            username: <%=twitterMessageDto.getUsername()%>
+        </div>
+    </li>
+    <%
+        }
+    %>
+</ul>
+</body>
+</html>
