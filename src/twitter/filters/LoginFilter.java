3ce0e684e01027d87d78fc6b41de5e0de594c61b
miszalemko <lemkomisza@gmail.com>
2018-03-17 14:00:24 +0100

    adding redirect whne you had not logged

--- src/twitter/filters/LoginFilter.java
+++ src/twitter/filters/LoginFilter.java
@@ -1,4 +1,34 @@
 package twitter.filters;
 
-public class LoginFilter {
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(value = {"/tweet","/addTweet"})
+public class LoginFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+
+        if(session.getAttribute("username")==null) {
+            response.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
